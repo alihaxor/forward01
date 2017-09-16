85f30ba49f08ce9cb4074bbd94949422819297fe
Yusuf Ali <md.yousuf844@yahoo.com>
2017-09-17 02:47:48 +0600

    webpack config

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
 ReactDOM.render(
-    React.createElement("h2", null, "Hello React"),
+    <h2>Hello JSX</h2>,
     document.getElementById("root")
 );
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'client'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
